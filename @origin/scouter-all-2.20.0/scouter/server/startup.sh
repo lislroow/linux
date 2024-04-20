4484cbc1f39a87647ee1589c85953571744067bb
김명구 <myeonggu.kim@kakao.com>
2024-04-20 15:38:54 +0900

    scouter-server java8

--- @origin/scouter-all-2.20.0/scouter/server/startup.sh
+++ @origin/scouter-all-2.20.0/scouter/server/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nohup java -Xmx1024m -classpath ./scouter-server-boot.jar scouter.boot.Boot ./lib > nohup.out &
+nohup /c/develop/tools/java/corretto-1.8.0_382/bin/java -Xmx1024m -classpath ./scouter-server-boot.jar scouter.boot.Boot ./lib > nohup.out &
 sleep 1
 tail -100 nohup.out
 
